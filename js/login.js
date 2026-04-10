07cc8abd6297545bc6c3cd5deb86fa3eb612c50f
kiktipong <kiktipong@users.noreply.github.com>
2026-04-10 06:15:13 +0000

    deploy: 2ce4763fff05fbfe49b58e63ee8edfb1d485f2c3

--- js/login.js
+++ js/login.js
@@ -1,10 +1,17 @@
-function login() {
-  const user = document.getElementById("username").value;
-  const pass = document.getElementById("password").value;
+function login(){
 
-  if (user === "admin" && pass === "1234") {
-    document.getElementById("result").innerText = "Login success ✅";
-  } else {
-    document.getElementById("result").innerText = "Login failed ❌";
+  let user = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+
+  let result = document.getElementById("result");
+
+  if(user === "admin" && pass === "1234"){
+    result.style.color = "green";
+    result.innerText = "Login Success ✅";
   }
+  else{
+    result.style.color = "red";
+    result.innerText = "Username or Password incorrect ❌";
+  }
+
 }
